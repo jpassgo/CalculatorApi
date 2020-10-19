6572df65786002245590f43eeed1bbf0372c591b
jpassgo <jeffpascoe9@gmail.com>
2020-10-18 20:46:44 -0400

    formatting

--- CalculatorRestApi/views.py
+++ CalculatorRestApi/views.py
@@ -30,7 +30,9 @@ def difference(request):
 @csrf_exempt
 def multiply(request):
     request_body = json.loads(request.body)
-    product_of_two_operands = product_of_operands(request_body['first_operand'], request_body['second_operand'])
+    product_of_two_operands = product_of_operands(
+        request_body['first_operand'],
+        request_body['second_operand'])
     return HttpResponse(
         json.dumps({'product': product_of_two_operands}),
         content_type="application/json"
@@ -51,6 +53,7 @@ def sum_operands(first_operand, second_operand):
 
 def difference_of_operands(first_operand, second_operand):
     return float(first_operand) - float(second_operand)
+
 
 def product_of_operands(first_operand, second_operand):
     return float(first_operand) * float(second_operand)
